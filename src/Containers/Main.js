f64e2770dee79d64c0a634a4d17d535db3208f33
vilas-m <vilas@humancloud.co.in>
2021-04-29 17:22:25 +0530

    Added containers

---
+++ src/Containers/Main.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./styles.scss";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="w-content-small">
+        <div className="top"></div>
+        <div className="middle"></div>
+        <div className="bottom"></div>
+      </div>
+      <div className="w-content">
+        <div className="top"></div>
+        <div className="middle"></div>
+        <div className="bottom"></div>
+      </div>
+      <div className="w-content-small">
+        <div className="top"></div>
+        <div className="middle"></div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
+
+// 768
+// 153 20%
+// 467 60%
+// 150 20%
+
+// 448
+// 53 12%
+// 209  46%
+// 186 42%
